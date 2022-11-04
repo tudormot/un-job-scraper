0d5540effdb17d78fa43a400c0daa423d5aae59a
tudor <mot.tudor@yahoo.com>
2022-11-04 11:44:27 +0100

    added debug vers of program

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+from src.storage.tiny_db_dao import TinyDBDAO
 from src.usecases.delete_all_jobs_from_website_usecase import delete_all_jobs
 from src.usecases.usecases import scrape_all_website_usecase, \
     scrape_since_last_update_usecase
@@ -5,4 +6,5 @@ from src.config.logging_config import config_log
 
 
 config_log()
-scrape_since_last_update_usecase()
+db_dao = TinyDBDAO('db.json')
+# scrape_since_last_update_usecase()
